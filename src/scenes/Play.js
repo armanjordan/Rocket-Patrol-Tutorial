2ca24c811f2bd22a7faaad4540319883d95b936d
Arman Jordan <arajorda@ucsc.edu>
2022-04-18 20:56:52 -0700

    added faster ship

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,7 @@ preload() {
     this.load.image('rocket', './assets/ball.png');
     this.load.image('spaceship', './assets/ship.png');
     this.load.image('waves', './assets/wavesbg.png');
+    this.load.image('speedship', './assets/speedship.png');
     // load spritesheet
     this.load.spritesheet('explosion', './assets/sinkspritesheet.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9})
 }
@@ -27,9 +28,11 @@ preload() {
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
         // add spaceships (x3)
-        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
-        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
-        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30, game.settings.spaceshipSpeed).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20, game.settings.spaceshipSpeed).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10, game.settings.spaceshipSpeed).setOrigin(0, 0);
+
+        this.speedship = new Spaceship(this, game.config.width, borderUISize*3, 'speedship', 0, 50, (game.settings.spaceshipSpeed + 2)).setOrigin(0, 0);
 
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -90,6 +93,7 @@ preload() {
             this.ship01.update();
             this.ship02.update();
             this.ship03.update();
+            this.speedship.update();
         }
 
         // check collisions
@@ -104,6 +108,10 @@ preload() {
         if(this.checkCollision(this.p1Rocket, this.ship01)){
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
+        }
+        if(this.checkCollision(this.p1Rocket, this.speedship)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.speedship);
         }
     }
 

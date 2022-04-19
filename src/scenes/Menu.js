a77efb4ce1b10629cea2e1789a243bea83a0e6c9
Arman Jordan <arajorda@ucsc.edu>
2022-04-18 20:38:04 -0700

    all sounds

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,8 +5,8 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_select', './assets/button-21.wav');
+        this.load.audio('sfx_explosion', './assets/Explosion2.wav');
         this.load.audio('sfx_rocket', './assets/cannon.wav');
     }
 

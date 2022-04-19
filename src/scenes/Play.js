bf2794b2effb417b7c5f5987b385cf4a96c91456
Arman Jordan <arajorda@ucsc.edu>
2022-04-18 20:20:56 -0700

    cannon sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,13 +15,13 @@ preload() {
     create() {
         // place tile sprite
         this.waves = this.add.tileSprite(0, 0, 640, 480, 'waves').setOrigin(0, 0);
-        // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-        // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // yellow UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xFFFF00).setOrigin(0, 0);
+        // blue borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x0000FF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0x0000FF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0x0000FF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x0000FF).setOrigin(0, 0);
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);

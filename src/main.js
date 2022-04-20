523d1c086a5951d911feafe6910aa056c02cd090
Arman Jordan <arajorda@ucsc.edu>
2022-04-19 20:54:38 -0700

    included necessary comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+// -------Title-------
+// Arman Jordan
+// Rocket Patrol Mods
+// 4/19/2022
+// 10-11 hours
+//
+// -------Point Breakdown-------
+//
+// I changed the sprites, background, sound, and UI to support a new theme of shooting cannon
+// balls towards enemy ships in the ocean(60 points)
+//
+// I added a 4th faster ship that is worth more points(20 points)
+//
+// I added a simultaneous 2nd player that is controlled by using A and D to move left and
+// right, and the X button to fire (30 points)
+//
+//----------------------------
+
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
